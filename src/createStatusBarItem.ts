b69399cefd8fae5c9a1a9b5377c089b800c8339c
gobbedy <gperr050@uottawa.ca>
2023-02-20 13:13:05 -0500

    Update createStatusBarItem.ts

--- src/createStatusBarItem.ts
+++ src/createStatusBarItem.ts
@@ -8,10 +8,7 @@ const TOOLTIP_TEXT = 'Enable/Disable formatting'
 
 const createStatusBarItem = () => {
   const statusBarItem = window.createStatusBarItem(
-    StatusBarAlignment.Right,
-    // We substract one so that it is always to the right of the Prettier status
-    // bar item. If we didn’t do this, the position could switch randomly.
-    PRETTIER_STATUS_BAR_PRIORITY - 1,
+    StatusBarAlignment.Left
   )
 
   statusBarItem.command = COMMAND_NAME
